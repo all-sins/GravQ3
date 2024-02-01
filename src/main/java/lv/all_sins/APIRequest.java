442528200520a946efb8ad44454e2e1e7349ad64
unknown <tsukiakari@tutanota.com>
2024-02-01 21:43:08 +0200

    Added converstion between OffsetDateTime and UnixMillis and started to rebuild APIRequest class.

--- src/main/java/lv/all_sins/APIRequest.java
+++ src/main/java/lv/all_sins/APIRequest.java
@@ -1,4 +1,48 @@
 package lv.all_sins;
 
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+
 public class APIRequest {
+    private final String apiEndpoint;
+    private final String apiTarget;
+    public final String urlParamStartSymbol = "?";
+    public final String urlParamDelimiter = "&";
+    private final ArrayList<URLParameter> urlParams = new ArrayList<>();
+
+    public APIRequest(String apiEndpoint, String apiTarget) {
+        this.apiEndpoint = apiEndpoint;
+        this.apiTarget = apiTarget;
+    }
+
+    public OffsetDateTime unixMillisToOffsetDateTime(long unixTimeMillis) {
+        Instant instant = Instant.ofEpochMilli(unixTimeMillis);
+        return instant.atOffset(ZoneOffset.UTC);
+    }
+
+    public long offsetDateTimeToUnixMillis(OffsetDateTime offsetDateTime) {
+        return offsetDateTime.toInstant().toEpochMilli();
+    }
+
+    public String getApiEndpoint() {
+        return apiEndpoint;
+    }
+
+    public String getApiTarget() {
+        return apiTarget;
+    }
+
+    public ArrayList<URLParameter> getUrlParams() {
+        return urlParams;
+    }
+
+    public void addUrlParam(URLParameter urlParameter) {
+        urlParams.add(urlParameter);
+    }
+
+    public void clearUrlParams() {
+        urlParams.clear();
+    }
 }
--- src/main/java/lv/all_sins/Main.java
+++ src/main/java/lv/all_sins/Main.java
@@ -1,7 +1,30 @@
 package lv.all_sins;
 
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String apiEndpoint = "https://api.binance.com";
+        String apiTarget = "/api/v3/aggTrades";
+        String spotPairSymbol = "ETHUSDT";
+        int pageMax = 1000;
+
+        String startDateString = "2023-01-01 00:00:00:000 Z";
+        String endDateString = "2023-01-01 23:59:59:999 Z";
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss:SSS zzz");
+
+        OffsetDateTime parsedStartDate = OffsetDateTime.parse(startDateString, formatter.withZone(ZoneId.of("UTC")));
+        OffsetDateTime parsedEndDate = OffsetDateTime.parse(endDateString, formatter.withZone(ZoneId.of("UTC")));
+
+        APIRequest apiRequest = new APIRequest(apiEndpoint, apiTarget);
+
+        long startUnixMillis = apiRequest.offsetDateTimeToUnixMillis(parsedStartDate);
+        long endUnixMillis = apiRequest.offsetDateTimeToUnixMillis(parsedEndDate);
+
+        System.out.println(startUnixMillis);
+        System.out.println(endUnixMillis);
     }
 }
